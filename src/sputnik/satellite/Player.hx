fd09e07a432a4701e51787cbde4cc4da03c6a1fa
Lucas A Jones <Necxii@gmail.com>
2015-03-03 13:50:33 +0000

    Added technology stats

--- src/sputnik/satellite/Player.hx
+++ src/sputnik/satellite/Player.hx
@@ -2,6 +2,58 @@ package sputnik.satellite;
 
 using Reflect;
 
+class TechStats
+{
+	public var value:Float;
+	public var level:Int;
+
+	public static function fromDynamic(data:Dynamic):TechStats
+	{
+		var techStats = new TechStats();
+
+		techStats.value = data.field("value");
+		techStats.level = data.field("level");
+
+		return techStats;
+	}
+
+	public function new()
+	{
+
+	}
+}
+
+class Technology
+{
+	public var scanning:TechStats;
+	public var propulsion:TechStats;
+	public var terraforming:TechStats;
+	public var research:TechStats;
+	public var weapons:TechStats;
+	public var banking:TechStats;
+	public var manufacturing:TechStats;
+
+	public static function fromDynamic(data:Dynamic):Technology
+	{
+		var technology = new Technology();
+
+		technology.scanning = TechStats.fromDynamic(data.field("scanning"));
+		technology.propulsion = TechStats.fromDynamic(data.field("propulsion"));
+		technology.terraforming = TechStats.fromDynamic(data.field("terraforming"));
+		technology.research = TechStats.fromDynamic(data.field("research"));
+		technology.weapons = TechStats.fromDynamic(data.field("weapons"));
+		technology.banking = TechStats.fromDynamic(data.field("banking"));
+		technology.manufacturing = TechStats.fromDynamic(data.field("manufacturing"));
+
+		return technology;
+	}	
+
+	public function new()
+	{
+
+	}
+}
+
 class Player
 {
 	public var uid:Int;
@@ -14,6 +66,8 @@ class Player
 	public var industry:Int;
 	public var science:Int;
 	public var economy:Int;
+
+	public var technology:Technology;
 
 	public static function fromDynamic(data:Dynamic):Player
 	{
@@ -29,6 +83,8 @@ class Player
 		player.industry = data.field("total_industry");
 		player.science = data.field("total_science");
 		player.economy = data.field("total_economy");
+
+		player.technology = Technology.fromDynamic(data.field("tech"));
 
 		return player;
 	}
